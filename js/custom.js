2b57acf1d5007542b96e91e9327805bcd91ed577
Tanvir_Ejaj <tanvirejajtushar@gmail.com>
2022-12-29 11:14:44 +0600

    fourth

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,4 @@
 $(function () {
-  // $(".ceo-main").slick({
-  //   infinite: true,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  // });
 
   $(".counter").counterUp({
     delay: 10,
@@ -43,5 +38,44 @@ $(function () {
   });
   $(".close").on("click", function () {
     $(".sidebar").css("left", "-100%");
+  });
+
+  $(".slide-1").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoPlay: true,
+    autoplaySpeed: 2000,
+    nextArrow: '<i class="fa-solid fa-caret-right arrow-right"></i>',
+    prevArrow: '<i class="fa-solid fa-caret-left arrow-left"></i>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoPlay: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoPlay: true,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoPlay: true,
+        },
+      },
+    ],
   });
 });
